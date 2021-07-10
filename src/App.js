71f1ff50fca80842315474b78b5de9e4ae74e561
uncle_femi <femi@coredatalytix.com>
2021-07-10 20:14:01 +0100

    Wrap Up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import { useEffect, useState } from 'react';
 import FoodCard from './components/foodCard';
 
+
+const Loader = () => (
+  <div className="w-full h-screen flex items-center justify-center">
+    <div
+      className="animate-spin ease-linear rounded-full border-4 border-t-4 border-gray-400 h-20 w-20"
+      style={{ borderTopColor: '#FA9E0D' }}
+    />
+  </div>
+);
+
 function App() {
   const [foods, setFoods] = useState([]);
   useEffect(() => {
@@ -10,21 +20,13 @@ function App() {
   }, []);
   console.log(foods)
   return (
-    <div className="container">
-      {foods.map((food) => <FoodCard key={food.id} food={food} />)}
+    <div className="w-10/12 mx-auto grid grid-cols-12 gap-2 md:gap-6 lg:gap-12 mt-6 md:mt-10">
+      {!!foods.length
+        ? foods.map((food) => <FoodCard key={food.id} food={food} />)
+        : <Loader />
+      }
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
-// const Loader = () => (
-  //   <div
-  //     className="loader"
-  //     style={{ borderTopColor: '#FA9E0D' }}
-  //   />
-  // );
--- src/components/foodCard.js
+++ src/components/foodCard.js
@@ -1,8 +1,30 @@
-function FoodCard({food}) {
+import StarRatings from 'react-star-ratings';
+
+function FoodCard({ food }) {
   return (
-    <div className="card">
-      <h2>{food.strMeal}</h2>
-      <img src={food.strMealThumb} alt={food.strMeal}/>
+    <div className="card flex flex-col items-center col-span-12 sm:col-span-6 lg:col-span-4 my-5 md:my-10 pt-10 shadow-lg">
+      <img src={food.strMealThumb} alt={food.strMeal} className="rounded-lg card-img" />
+      <div className="flex justify-between w-full px-4 mt-6 text-2xl font-bold theme-black">
+        <h1>{food.title}</h1>
+        <h2>{food.price}</h2>
+      </div>
+      <h2 className="w-full px-4 text-sm font-semibold theme-grey">{food.strMeal}</h2>
+      <p className="w-full px-4 text-sm font-semibold theme-grey mt-4">
+        {`${food.description.substring(0, 142)}...`}
+      </p>
+      <div className="flex justify-between items-center w-full pl-4 mt-6">
+        <div className="w-48">
+          <StarRatings
+            rating={food.ratings * (4/6)}
+            starRatedColor="#FA9E0D"
+            numberOfStars={4}
+            name='rating'
+            starDimension="20px"
+            starSpacing="0.5px"
+          />
+        </div>
+        <button className="card-btn theme-background py-0 px-10 text-white font-light"> + </button>
+      </div>
     </div>
   )
 }
